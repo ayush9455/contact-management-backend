cf2da8c8cea0c77e572d8e77b4babb8e142559c2
Ayush Gupta <ayush.gupta@brihatinfotech.com>
2025-02-04 23:59:52 +0530

    vercel config update

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const dotenv = require("dotenv").config()
 connectDb();
 const app = express();
 const port = process.env.PORT || 5002;
+
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
+app.get('/', (req, res) => {
+    res.json({ message: "Server is Live" });
+});
 app.use(errorHandler)
 app.listen(port, () => {
     console.log(`Server is Running on port ${port}`);
